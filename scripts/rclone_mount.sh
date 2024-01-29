1a216ea17dcd01707c01a66169daabe2cabf45a0
Luis Meier <luis.meier@ost.ch>
2024-01-29 18:21:47 +0100

    Added scripts folder

---
+++ scripts/rclone_mount.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+function ask_mount
+{
+  read -p "What to mount? [P]ersonal, [T]eam, [A]ll, [Q]uit" type
+  case $type in 
+    [Pp]* )
+      echo "Mounting Personal"
+      rclone mount OST-OneDrive: /home/luism/OST-OneDrive/personal/ --vfs-cache-mode full --daemon
+      ;;
+    [Tt]* ) 
+      echo "Mounting Team"
+      rclone mount OST-SharePoint: /home/luism/OST-OneDrive/TeamDejan  --vfs-cache-mode full --daemon
+      ;;
+    [Aa]* )
+      echo "Mounting both"
+      rclone mount OST-SharePoint: /home/luism/OST-OneDrive/TeamDejan  --vfs-cache-mode full --daemon
+      rclone mount OST-OneDrive: /home/luism/OST-OneDrive/personal/ --vfs-cache-mode full --daemon
+      exit
+      ;;
+    [Qq]* )
+      ;;
+    *) echo "Not an option"
+  esac
+}
+
+function ask_umount
+{
+  read -p "What to unmount? [P]ersonal, [T]eam, [A]ll, [Q]uit" type
+  case $type in 
+    [Pp]* )
+      echo "unmounting Personal"
+      fusermount -u /home/luism/OST-OneDrive/personal 
+      ;;
+    [Tt]* ) 
+      echo "unmounting Team"
+      fusermount -u /home/luism/OST-OneDrive/TeamDejan
+      ;;
+    [Aa]* )
+      echo "unmounting all\n"
+      fusermount -u /home/luism/OST-OneDrive/personal 
+      fusermount -u /home/luism/OST-OneDrive/TeamDejan
+      exit
+      ;;
+    [Qq]* )
+      ;;
+    *) echo "Not an option"
+  esac
+}
+
+while true; do
+  read -p "[M]ount or [u]nmount?[Q]uit" mount_type
+  case $mount_type in 
+    [Mm]* )
+      echo "Mount selected"
+      ask_mount
+      ;;
+    [Uu]* ) 
+      echo "Unmount selected"
+      ask_umount
+      ;;
+    [Qq]* )
+      echo "Exiting"
+      exit
+      ;;
+
+    *) echo "Not an option"
+  esac
+done
+
+
