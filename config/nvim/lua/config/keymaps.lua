546261580e8ccd240a6e73f3d6349751ced2279f
Luis Meier <luis.meier@ost.ch>
2024-02-07 09:02:03 +0100

    Nvim: cfg updates

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- vim.keymap.set({ "n", "v" }, "<leader>\\", function()
+--  require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+-- end)
+
+-- maps.n["<leader>/"] = {
+-- function() end,
+--   desc = "Toggle comment line",
+-- }
+-- maps.v["<leader>/"] = {
+--   "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+--   desc = "Toggle comment for selection",
+-- }
