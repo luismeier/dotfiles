8fb732ba89153b9c86fdc9d7085304032af18fae
Luis Meier <luis.meier@ost.ch>
2024-04-05 10:58:43 +0200

    Rofi add power menu

---
+++ config/rofi/powermenu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries | rofi -dmenu | awk '{print tolower($2)}')
+
+case $selected in
+logout)
+	hyprctl dispatch exit
+	;;
+suspend)
+	exec systemctl suspend
+	;;
+reboot)
+	exec systemctl reboot
+	;;
+shutdown)
+	exec systemctl poweroff
+	;;
+	# it used to be poweroff -i
+esac
