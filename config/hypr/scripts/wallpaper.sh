fed7666fe77ae7d2d9961bf521730ba5429ddb0b
Luis Meier <luis.meier@ost.ch>
2024-07-11 10:49:26 +0200

    Hypr: Fixes and cleanup

--- config/hypr/scripts/wallpaper.sh
+++ config/hypr/scripts/wallpaper.sh
@@ -4,12 +4,15 @@ directory=~/Pictures/wallpapers
 monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
 if [ -d "$directory" ]; then
-	random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
-	echo $random_background
-	echo $monitor
-	hyprctl hyprpaper unload all
-	hyprctl hyprpaper preload $random_background
-	for m in ''${monitor[@]}; do
-		hyprctl hyprpaper wallpaper "$m, $random_background"
-	done
+  random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
+  echo $random_background
+  echo $monitor
+  echo -n 'unloading old: '
+  hyprctl hyprpaper unload all
+  echo -n 'loading new: '
+  hyprctl hyprpaper preload $random_background
+  for m in ''${monitor[@]}; do
+    echo -n "setting $m: "
+    hyprctl hyprpaper wallpaper "$m, $random_background"
+  done
 fi
