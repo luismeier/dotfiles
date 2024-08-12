bbf8d9c3ac0ffbdf72d22c7e0385b8f7b199db8e
Luis Meier <luis.meier@ost.ch>
2024-08-12 09:01:23 +0200

    Hypr: Multiple updates

--- config/hypr/scripts/wallpaper.sh
+++ config/hypr/scripts/wallpaper.sh
@@ -5,12 +5,13 @@ monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
 if [ -d "$directory" ]; then
   random_background=$(ls $directory/*.{jpg,png} | shuf -n 1)
-  echo $random_background
-  echo $monitor
+  echo "$random_background"
+  echo "$monitor"
+  ln -sf "$random_background" /home/luism/.config/current_wallpaper
   echo -n 'unloading old: '
   hyprctl hyprpaper unload all
   echo -n 'loading new: '
-  hyprctl hyprpaper preload $random_background
+  hyprctl hyprpaper preload "$random_background"
   for m in ''${monitor[@]}; do
     echo -n "setting $m: "
     hyprctl hyprpaper wallpaper "$m, $random_background"
