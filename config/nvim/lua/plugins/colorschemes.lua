6d2b808c8fb6831da2c2d5e9e1aa7ec72435bf7b
Luis Meier <luis.meier@ost.ch>
2024-10-22 16:14:42 +0200

    Update nvim config

--- config/nvim/lua/plugins/colorschemes.lua
+++ config/nvim/lua/plugins/colorschemes.lua
@@ -1,34 +1,38 @@
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   -- config = function()
+  --   --   require("catppuccin").setup({
+  --   --     no_italic = false,
+  --   --     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+  --   --       comments = { "italic" }, -- Change the style of comments
+  --   --       conditionals = { "italic" },
+  --   --     },
+  --   --   })
+  --   -- end,
+  -- },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   priority = 1000,
+  --   -- config = function()
+  --   --   require("nightfox").setup({
+  --   --     options = {
+  --   --       styles = {
+  --   --         comments = "italic",
+  --   --         conditionals = "italic",
+  --   --         keywords = "italic,bold",
+  --   --         types = "italic,bold",
+  --   --         operators = "bold",
+  --   --         variables = "bold",
+  --   --       },
+  --   --     },
+  --   --   })
+  --   -- end,
+  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "navarasu/onedark.nvim",
     priority = 1000,
-    -- config = function()
-    --   require("catppuccin").setup({
-    --     no_italic = false,
-    --     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-    --       comments = { "italic" }, -- Change the style of comments
-    --       conditionals = { "italic" },
-    --     },
-    --   })
-    -- end,
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    priority = 1000,
-    -- config = function()
-    --   require("nightfox").setup({
-    --     options = {
-    --       styles = {
-    --         comments = "italic",
-    --         conditionals = "italic",
-    --         keywords = "italic,bold",
-    --         types = "italic,bold",
-    --         operators = "bold",
-    --         variables = "bold",
-    --       },
-    --     },
-    --   })
-    -- end,
   },
 }
