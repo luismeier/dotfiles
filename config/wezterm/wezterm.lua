8d8af4071c1fa5d7341abbb0988be4c404206ca5
Luis Meier <meierluis@hotmail.de>
2025-01-08 10:53:28 +0100

    Set wezterm as default terminal

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,12 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
--- config.color_scheme = "AdventureTime"
 -- config.color_scheme = "Tokyo Night"
 config.color_scheme = "catppuccin-macchiato"
 
-config.font = wezterm.font("Fira Code NerdFont mono")
-config.font_size = 11
+-- config.font = wezterm.font("Fira Code NerdFont mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 12
 
 config.enable_tab_bar = false
 
