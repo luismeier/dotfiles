8b7cc68d620c7b634c95e35e9cc109c620ee2b76
Luis Meier <meierluis@hotmail.de>
2025-06-24 19:28:57 +0200

    Waybar: better nvidia module

---
+++ config/waybar/scripts/nvidia_status.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Check the line “Suspended” vs “Active” in supergfxctl output
+state=$(supergfxctl -S)
+
+if grep -q "Suspended" <<<"$state"; then
+  printf '{"icon":" ","text":"","class":"suspended"}'
+else
+  printf '{"icon":" ","text":"","class":"active"}'
+fi
