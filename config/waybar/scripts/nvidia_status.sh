7fb26c13af421a2382c7cdea54300624c9054465
Luis Meier <meierluis@hotmail.de>
2025-06-24 20:27:28 +0200

    Waybar: fixed nvid, smaller margins

--- config/waybar/scripts/nvidia_status.sh
+++ config/waybar/scripts/nvidia_status.sh
@@ -3,7 +3,7 @@
 # Check the line “Suspended” vs “Active” in supergfxctl output
 state=$(supergfxctl -S)
 
-if grep -q "Suspended" <<<"$state"; then
+if grep -q "suspended" <<<"$state"; then
   printf '{"icon":" ","text":"","class":"suspended"}'
 else
   printf '{"icon":" ","text":"","class":"active"}'
