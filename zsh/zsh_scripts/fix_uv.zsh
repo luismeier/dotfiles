a308c0f6fdbad470222bcf03409f182dcec05d6d
Luis Meier <meierluis@hotmail.de>
2025-07-24 09:15:04 +0200

    zsh: fixes for uv completion

--- zsh/zsh_scripts/fix_uv.zsh
+++ zsh/zsh_scripts/fix_uv.zsh
@@ -1,21 +1,30 @@
-if ! type uv > /dev/null; then
-  echo "No uv"
-  return
-fi
-eval "$(uv generate-shell-completion zsh)"
-
+# Fix completions for uv run to autocomplete .py files
 _uv_run_mod() {
     if [[ "$words[2]" == "run" && "$words[CURRENT]" != -* ]]; then
-        local venv_binaries
-        if [[ -d .venv/bin ]]; then
-            venv_binaries=( ${(@f)"$(_call_program files ls -1 .venv/bin 2>/dev/null)"} )
-        fi
-        
-        _alternative \
-            'files:filename:_files' \
-            "binaries:venv binary:(($venv_binaries))"
+        _arguments '*:filename:_files -g "*.py"'
     else
         _uv "$@"
     fi
 }
 compdef _uv_run_mod uv
+# if ! type uv > /dev/null; then
+#   echo "No uv"
+#   return
+# fi
+# eval "$(uv generate-shell-completion zsh)"
+#
+# _uv_run_mod() {
+#     if [[ "$words[2]" == "run" && "$words[CURRENT]" != -* ]]; then
+#         local venv_binaries
+#         if [[ -d .venv/bin ]]; then
+#             venv_binaries=( ${(@f)"$(_call_program files ls -1 .venv/bin 2>/dev/null)"} )
+#         fi
+#
+#         _alternative \
+#             'files:filename:_files' \
+#             "binaries:venv binary:(($venv_binaries))"
+#     else
+#         _uv "$@"
+#     fi
+# }
+# compdef _uv_run_mod uv
