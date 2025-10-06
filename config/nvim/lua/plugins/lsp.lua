5b6c2081f019319bb280f874bb8f7d4e42ce9b0c
Luis Meier <meierluis@hotmail.de>
2025-10-06 17:15:04 +0200

    Lazy: Update

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
+  opts = function(_, opts)
+    local lspconfig = require("lspconfig")
+
+    -- Add your keymap
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- change a keymap
     keys[#keys + 1] = {
       "gV",
       function()
         vim.cmd("vsplit")
-        -- Snacks.picker.lsp_definitions()
         vim.lsp.buf.definition()
       end,
       desc = "Open definition in split",
