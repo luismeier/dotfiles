77a54dac840ca729b176605f23aec12d7440e05a
Luis Meier <meierluis@hotmail.de>
2026-01-06 12:52:57 +0100

    Nvim: plugin cleanup

--- config/nvim/lua/config/devcontainer.lua
+++
@@ -1,4 +0,0 @@
-local lspconfig_util = require('lspconfig.util')
-lspconfig_util.on_setup = lspconfig_util.add_hook_after(lspconfig_util.on_setup, function(config, user_config)
-  config.on_new_config = lspconfig_util.add_hook_after(config.on_new_config, require('devcontainers').on_new_config)
-end)
